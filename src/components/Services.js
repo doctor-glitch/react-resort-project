06f08ace46f755789591add455734dd5155c39c3
Harin Rajan <52859561+doctor-glitch@users.noreply.github.com>
2019-10-04 13:34:18 +0530

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
+import { FaCocktail, FaHiking, FaShuttleVan, FaMugHot } from "react-icons/fa";
 import Title from './Title'
 
 //for listing out all services
@@ -12,19 +12,19 @@ export default class Services extends Component {
         info: ' Our diabetes-friendly mocktail recipes use a variety of ingredients including fresh fruit, club soda, and sugar substitutes. Enjoy our favorite skinny cocktails - alcoholic and nonalcoholic - that everyone will love. ',
       },
       {
-        icon: <FaBeer />,
-        title: "Free Beer",
-        info: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
+        icon: <FaMugHot />,
+        title: "Free Coffee",
+        info: 'We believe that coffee is more than just a drink: It’s a culture, an economy, an art, a science — and a passion. Whether you\'re new to the brew or an espresso expert, there\'s always more to learn about this beloved beverage ',
       },
       {
         icon: <FaHiking />,
         title: "Free hiking",
-        info: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
+        info: 'Hiking, walking in nature as a recreational activity. Especially among those with sedentary occupations, hiking is a natural exercise that promotes physical fitness, is economical and convenient, and requires no special equipment.'
       },
       {
         icon: <FaShuttleVan />,
         title: "Free Travel",
-        info: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
+        info: 'These days there are many ways to travel the world for free, or at least ultra cheap. Some you’ll know about, some you won’t, lets take a look at working your way around the world and more, for free or super budget. ',
       },
     ]
   }
