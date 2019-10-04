04bb55b94ff650d83b8570286adc97b4764469d7
Harin Rajan <52859561+doctor-glitch@users.noreply.github.com>
2019-10-04 12:28:23 +0530

    Update Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Error = () => {
   return (
     <Hero>
-      <Banner title ="404" subtitle="Page Not Found">
+      <Banner title ="404" subtitle="Page Not Found!!!!">
         <Link to='/' className="btn-primary">
           Return Home
         </Link>
