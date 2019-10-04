ba9e043cbe8fe471f16f1f178e953d439ac2cea8
Harin Rajan <52859561+doctor-glitch@users.noreply.github.com>
2019-10-04 13:54:51 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,9 @@ import Banner from '../components/Banner'
 import { Link } from "react-router-dom";
 import Services from '../components/Services'
 import FeaturedRooms from '../components/FeaturedRooms'
+import insta from '../images/insta.png'
+import fb from '../images/fb.png'
+import ln from '../images/ln.png'
 
 const Home = () => {
  
@@ -23,6 +26,18 @@ const Home = () => {
         <h6>Designed and Developed by Harin
         <span dangerouslySetInnerHTML={{ "__html": "&copy;" }} />2019
         </h6>
+        <div id="contacts">
+          <h1><span>Folow me</span></h1>
+          <a href="https://www.instagram.com/the_photogenic_glitch/"target="_blank">
+            <img alt="Instagram" src={insta} />
+          </a>
+          <a href="https://www.facebook.com/harin.rajan" target="_blank">
+            <img alt="Facebook" src={fb} />
+          </a>
+          <a href="https://www.linkedin.com/in/harin-rajan-877249166/" target="_blank">
+            <img alt="Linkedin" src={ln} />
+          </a>
+        </div>
       </div>
     </>
   )
